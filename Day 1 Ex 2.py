5fb427cae685cd884bf69774229a25cb3ab3b066
TheBawawa <anagabrielaparedes324@gmail.com>
2022-07-05 13:08:10 -0500

    Day 1

---
+++ Day 1 Ex 2.py
@@ -0,0 +1,7 @@
+
+years = 27
+months = 12*years
+weeks = 4*months
+days = 7*weeks
+
+print(f"There is {days} days, {weeks} weeks and {months} months in {years} years.")
---
+++ Day 1 Ex 3.py
@@ -0,0 +1,5 @@
+
+r = 5
+pi = 3.14
+a = pi*(r*r)
+print(a)
